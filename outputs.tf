3a027eab271e2509f3aa043153cd1d5e1c7d5b47
testt <tapeshmz10403@outlook.com>
2025-06-28 09:59:05 +0200

    update fix 2

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
-output "host_pool_id" {
-  value = azurerm_virtual_desktop_host_pool.host_pool.id
-}
+# output "host_pool_id" {
+#   value = azurerm_virtual_desktop_host_pool.host_pool.id
+# }
 
-output "workspace_id" {
-  value = azurerm_virtual_desktop_workspace.workspace.id
-}
+# output "workspace_id" {
+#   value = azurerm_virtual_desktop_workspace.workspace.id
+# }
