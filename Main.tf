1b203c2de17bced91b30cd1e8f56bf687349a063
testt <tapeshmz10403@outlook.com>
2025-06-28 10:41:43 +0200

    fix7

--- Main.tf
+++ Main.tf
@@ -1,5 +1,5 @@
 module "avd_host_pools" {
-  source = "git::https://github.com/Tapeshmz10403/terraform-azure-avd-virtual-desktop-host-pool.git?ref=v1.0.6"
+  source = "git::https://github.com/Tapeshmz10403/terraform-azure-avd-virtual-desktop-host-pool.git?ref=v1.0.7"
 
   resource_group_name = var.resource_group_name
   location            = var.location
